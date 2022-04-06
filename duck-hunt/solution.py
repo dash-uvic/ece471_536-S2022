145723105feaad3ff617e87d7a7cacfa3c9e0447
ChristopherRDunn <crdunn@shaw.ca>
2022-04-05 23:23:43 -0700

    Cleanup

--- duck-hunt/solution.py
+++ duck-hunt/solution.py
@@ -11,9 +11,9 @@ Two random coordinate generator has been provided for testing purposes.
 Manual mode where you can use your mouse as also been added for testing purposes.
 """
 def GetLocation(move_type, env, current_frame):
+    # Using template matching, some images require diff shifting
     templates = []
-    weirdTemplates = []
-    for i in range(1, 7):
+    for i in range(1, 8):
         templates.append({"fileName": "./ims/a" + str(i) + ".png", "shift": True})
     for i in range(1, 4):
         templates.append({"fileName": "./ims/weird" + str(i) + ".png", "shift": False})
@@ -22,9 +22,15 @@ def GetLocation(move_type, env, current_frame):
     for template in templates:
         templateImg = cv2.imread(template["fileName"], 0)
         h, w = templateImg.shape
+        # This is a very innacurate, but fast method, so I'm trusting that in the sea of false positives, at some point it'll see each duck. Also TM_CCOEFF is just the one I found most accurate
+        # I'm using an RCNN in another branch, which is amazing at finding the ducks, but it's wayyyy too slow to predict which makes it effectively useless in this case
+        #   It's probaly due to my potato of a laptop, but this is a much surer bet
+        # Don't get me started on the blue ducks on light backgrounds though, 4 of my 10 templates are specifically for that case, but I still can't detect them
         result = cv2.matchTemplate(cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY), templateImg, cv2.TM_CCOEFF)
         minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
         location = maxLoc
+
+        # The weird shifting diff, mostly due to where the hit box is
         if template["shift"]:
             botRight = (int(location[1] + w), int(location[0]))
         else:
